c01d52fcca04bb33e485440fff88568fc96b420e
Muunatic <ichigoez0@gmail.com>
2023-01-03 03:45:06 +0700

    build: update interval data

--- src/AppAPI.tsx
+++ src/AppAPI.tsx
@@ -10,6 +10,7 @@ import { bearer } from './config';
 class spotifyAPI {
 
     /**
+     * @protected
      * @type {string} - OAuth must be string type
      */
     protected OAuth: string;
@@ -24,12 +25,13 @@ class spotifyAPI {
     /**
      * Get currently playing image
      * 
+     * @private
      * @async
      * @param {string} params - bearer key
      * @returns {string} image data
      */
     private async spotifyImage(params: string): Promise<string | void> {
-        await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        return await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -38,21 +40,31 @@ class spotifyAPI {
             }
         }).then((res) => {
             return res.json();
-        }).then((data): Promise<string> => {
-            let result = document.getElementById("Image") as HTMLImageElement | null;
-            return result.src = data.item.album.images[0].url;
+        }).then((data): string => {
+            if (data.item == null) {
+                return null;
+            } else {
+                let imgid = document.getElementById("Image") as HTMLImageElement | null;
+                let result = data.item.album.images[0].url;
+                if (result == imgid.src) {
+                    return;
+                } else if (result != imgid.src) {
+                    return imgid.src = result;
+                }
+            }
         });
     }
 
     /**
      * Get currently playing name
      * 
+     * @private
      * @async
      * @param {string} params - bearer key
      * @returns {string} name data
      */
     private async spotifyName(params: string): Promise<string | void> {
-        await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        return await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -61,23 +73,33 @@ class spotifyAPI {
             }
         }).then((res) => {
             return res.json();
-        }).then((data): Promise<string> => {
-            let nameid = document.getElementById("Name");
-            let altdoc = document.getElementById("Image") as HTMLImageElement | null;
-            altdoc.alt = data.item.name;
-            return nameid.innerHTML = data.item.name;
+        }).then((data): string => {
+            if (data.item == null) {
+                return null;
+            } else {
+                let nameid = document.getElementById("Name");
+                let altdoc = document.getElementById("Image") as HTMLImageElement | null;
+                let result = data.item.name;
+                if (result == nameid.innerHTML) {
+                    return;
+                } else if (result != nameid.innerHTML) {
+                    altdoc.alt = result;
+                    return nameid.innerHTML = result;
+                }
+            }
         });
     }
 
     /**
      * Get currently playing artist
      * 
+     * @private
      * @async
      * @param {string} params - bearer key
      * @returns {string} artist data
      */
     private async spotifyArtist(params: string): Promise<string | void> {
-        await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        return await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -86,21 +108,31 @@ class spotifyAPI {
             }
         }).then((res) => {
             return res.json();
-        }).then((data) => {
-            let nameid = document.getElementById("Artists");
-            return nameid.innerHTML = 'by ' + data.item.artists[0].name;
+        }).then((data): string => {
+            if (data.item == null) {
+                return null;
+            } else {
+                let artistid = document.getElementById("Artists");
+                let result = 'by ' + data.item.artists[0].name;
+                if (result == artistid.innerHTML) {
+                    return;
+                } else if (result != artistid.innerHTML) {
+                    return artistid.innerHTML = result;
+                }
+            }
         });
     }
 
     /**
      * Get currently playing album
      * 
+     * @private
      * @async
      * @param {string} params - bearer key
      * @returns {string} album data
      */
     private async spotifyAlbum(params: string): Promise<string | void> {
-        await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        return await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -109,21 +141,31 @@ class spotifyAPI {
             }
         }).then((res) => {
             return res.json();
-        }).then((data) => {
-            let nameid = document.getElementById("Album");
-            return nameid.innerHTML = 'on ' + data.item.album.name;
+        }).then((data): string => {
+            if (data.item == null) {
+                return null;
+            } else {
+                let nameid = document.getElementById("Album");
+                let result = 'on ' + data.item.album.name;
+                if (result == nameid.innerHTML) {
+                    return;
+                } else if (result != nameid.innerHTML) {
+                    return nameid.innerHTML = result;
+                }
+            }
         });
     }
 
     /**
      * Is user currently playing?
      * 
+     * @private
      * @async
      * @param {string} params - bearer key
      * @returns {boolean} boolean
      */
-    private async spotifyIsPlaying(params: string): Promise<boolean | void> {
-        await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+    private async spotifyIsPlaying(params: string): Promise<boolean> {
+        return await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -133,55 +175,70 @@ class spotifyAPI {
         }).then((res) => {
             return res.json();
         }).then((data) => {
-            let isPlaying = document.getElementById("IsPlaying");
-            let statusId = document.getElementById("spotifyuser");
-            if (data.is_playing == true) {
-                if (statusId.classList.contains("iconoffline")) {
-                    statusId.classList.remove("iconoffline");
-                    statusId.classList.add("icononline");
-                }
-                isPlaying.innerHTML = 'Now Playing';
-                this.spotifyImage(params);
-                this.spotifyName(params);
-                this.spotifyArtist(params);
-                this.spotifyAlbum(params);
-                return console.log(data.is_playing);
+            if (data.item == null) {
+                return null;
             } else {
-                if (statusId.classList.contains("icononline")) {
-                    statusId.classList.remove("icononline");
-                    statusId.classList.add("iconoffline");
+                let isPlaying = document.getElementById("IsPlaying");
+                let statusId = document.getElementById("spotifyuser");
+                if (data.is_playing == true) {
+                    if (statusId.classList.contains("iconoffline")) {
+                        statusId.classList.remove("iconoffline");
+                        statusId.classList.add("icononline");
+                    }
+                    isPlaying.innerHTML = 'Now Playing';
+                    this.spotifyImage(params);
+                    this.spotifyName(params);
+                    this.spotifyArtist(params);
+                    this.spotifyAlbum(params);
+                    return true;
+                } else if (data.is_playing == false) {
+                    if (statusId.classList.contains("icononline")) {
+                        statusId.classList.remove("icononline");
+                        statusId.classList.add("iconoffline");
+                    }
+                    isPlaying.innerHTML = 'Recently Played';
+                    this.spotifyImage(params);
+                    this.spotifyName(params);
+                    this.spotifyArtist(params);
+                    this.spotifyAlbum(params);
+                    return false;
                 }
-                isPlaying.innerHTML = 'Recently Played';
-                this.spotifyImage(params);
-                this.spotifyName(params);
-                this.spotifyArtist(params);
-                this.spotifyAlbum(params);
-                return console.log(data.is_playing);
             }
         });
     }
 
     /**
      * Start App
-     * 
+     * @public
      * @async
-     * @returns {boolean} boolean
+     * @returns {string} string
      */
-    async start(): Promise<boolean | void> {
-        setInterval(async () => {
-            if (await this.spotifyIsPlaying(this.OAuth) == true) {
-                this.spotifyImage(this.OAuth);
-                this.spotifyName(this.OAuth);
-                this.spotifyArtist(this.OAuth);
-                this.spotifyAlbum(this.OAuth);
-            } else {
-                console.error('Failed to start!');
-                return false;
-            } 
-        }, 5000);
+    public async start(): Promise<string | void> {
+        if (await this.spotifyIsPlaying(this.OAuth) == true) {
+            return console.log('Start');
+        } else if (await this.spotifyIsPlaying(this.OAuth) == false) {
+            return console.error('Failed to start!');
+        } 
     }
 
+    /**
+     * Update Data
+     * @public
+     * @async
+     * @returns {string} update data
+     */
+    public async update(): Promise<string | void> {
+        if (await this.spotifyIsPlaying(this.OAuth) == true) {
+            return console.log('Data updated');
+        } else if (await this.spotifyIsPlaying(this.OAuth) == false) {
+            return console.error('Failed to update data!');
+        } 
+    }
 }
 
 const api = new spotifyAPI(bearer);
-api.start();
+api.start().then(async () => {
+    setInterval(() => {
+        api.update();
+    }, 5000);
+})
